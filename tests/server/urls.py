e45c7d9b568115f7d67573af8f2925121dbcde28
Tom Clark <tom@octue.com>
2022-08-23 13:46:54 +0000

    DOC: Add a redirect to the root in the example app

--- tests/server/urls.py
+++ tests/server/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, re_path
+from django.views.generic.base import RedirectView
 
 from django_gcp import urls as django_gcp_urls
 
@@ -7,4 +8,8 @@ from django_gcp import urls as django_gcp_urls
 admin.autodiscover()
 
 
-urlpatterns = [re_path(r"^admin/", admin.site.urls), re_path(r"^test-django-gcp/", include(django_gcp_urls))]
+urlpatterns = [
+    re_path(r"^admin/", admin.site.urls),
+    re_path(r"^test-django-gcp/", include(django_gcp_urls)),
+    re_path(r"^", RedirectView.as_view(url="admin/")),
+]
