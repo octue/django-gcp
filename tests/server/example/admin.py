a6672ba8fb96d0341c878fab4b56839a9638e94a
Tom Clark <tom@octue.com>
2025-03-02 10:54:52 +0000

    FEA: Add readonly widget and complete demo in admin

--- tests/server/example/admin.py
+++ tests/server/example/admin.py
@@ -4,14 +4,18 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 from unfold import admin as unfold_admin
 
+from django_gcp.storage.admin import BlobFieldModelAdminMixin
+from django_gcp.storage.blob_utils import get_console_url, get_signed_download_url
 from tests.server.example.models import (
     ExampleBlankBlobFieldModel,
     ExampleBlobFieldModel,
+    ExampleCallbackBlobFieldModel,
     ExampleMultipleBlobFieldModel,
+    ExampleNeverOverwriteBlobFieldModel,
+    ExamplePathValidationBlobFieldModel,
+    ExampleReadOnlyBlobFieldModel,
     ExampleStorageModel,
-    ExampleUneditableBlobFieldModel,
 )
-
 
 # Allow switching between unfold and regular, for development purposes, by simply adding unfold to installed apps
 if "unfold" in settings.INSTALLED_APPS:
@@ -27,29 +31,109 @@ if "unfold" in settings.INSTALLED_APPS:
 else:
     ModelAdmin = django_admin.ModelAdmin
 
+from django import forms
+
+
+class DownloadAndConsoleButtonsForm(forms.ModelForm):
+    """Shows how to add download and console buttons to the widget"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance:
+            self.fields["blob"].widget.download_url = get_signed_download_url(self.instance, "blob")
+            self.fields["blob"].widget.console_url = get_console_url(self.instance, "blob")
+
+    class Meta:
+        model = ExampleBlobFieldModel
+        fields = "__all__"
+
 
 class ExampleStorageModelAdmin(ModelAdmin):
-    """A basic admin panel to demonstrate normal storage behaviour"""
-
-
-class ExampleBlobFieldModelAdmin(ModelAdmin):
-    """A basic admin panel to demonstrate the direct upload storage behaviour"""
+    """Demonstrates normal behaviour"""
 
 
 class ExampleBlankBlobFieldModelAdmin(ModelAdmin):
-    """A basic admin panel to demonstrate the direct upload storage behaviour where blank=True"""
+    """Demonstrates direct upload behaviour where null=True, blank=True"""
 
 
-class ExampleUneditableBlobFieldModelAdmin(ModelAdmin):
-    """A basic admin panel to demonstrate the direct upload storage behaviour where blank=True"""
+class ExampleBlobFieldModelAdmin(ModelAdmin):
+    """Demonstrates direct upload behaviour
+
+    This also demonstrates the ability to add a link to the GCP console
+    and a download button for the existing file.
+    (Note that the GCP console link turns the "Existing cloud path" text
+    into a link so there's no extra space used by another button, but it
+    does look the same).
+    """
+
+    form = DownloadAndConsoleButtonsForm
+
+
+class ExampleCallbackBlobFieldModelAdmin(ModelAdmin):
+    """Demonstrates direct upload behaviour where an on_change callback updates the activity field"""
 
 
 class ExampleMultipleBlobFieldModelAdmin(ModelAdmin):
-    """A basic admin panel to demonstrate the direct upload storage behaviour where multiple blobfields are used"""
+    """Demonstrates direct upload behaviour where multiple blobfields are used"""
+
+
+class ExampleNeverOverwriteBlobFieldModelAdmin(ModelAdmin):
+    """Demonstrates direct upload behaviour where overwrite_mode="never"."""
+
+
+class ExamplePathValidationBlobFieldModelAdmin(ModelAdmin):
+    """Demonstrates direct upload behaviour where a validation error is raised in the get_destination_name callback"""
+
+
+class ExampleReadOnlyBlobFieldModelAdmin(BlobFieldModelAdminMixin, ModelAdmin):
+    """Demonstrates how to show a read-only blob field in the admin
+
+    Django Admin is a nightmare when it comes to how it manages readonly
+    field rendering, it decides everything is text. So we have to do a lot of
+    botching. The least-difficult-to-understand way (I think) is to render
+    a completely separate field with a custom widget. We've supplied a template
+    to help with that (which is in itself botched because it has to work with
+    django's default container).
+
+    I made a mixin to contain the botching and simplify it a bit (see below).
+
+    Please participate in the following issue to help move on from this point:
+    https://forum.djangoproject.com/t/feature-request-discussion-custom-rendering-for-readonly-fields-in-admin/32009
+    """
+
+    # NOTE: You can't currently mix and match create_only_blob_fields and
+    # read_only_blob_fields, it works but there's something in the file
+    # selection that fails to work when the other field has been swapped out.
+    #
+    # See: https://github.com/octue/django-gcp/issues/76
+    #
+    # To reproduce, do this:
+    # fields = ["category", "example_read_only_blob", "example_create_only_blob"]
+    # create_only_blob_fields = ["example_create_only_blob"]
+    # read_only_blob_fields = ["example_read_only_blob"]
+
+    fields = ["category", "example_create_only_blob"]
+    readonly_fields = []  # Dont add them to admin's readonly_fields
+    create_only_blob_fields = ["example_create_only_blob"]
+
+    # You need to do this for each of the blob fields on the model
+    def example_create_only_blob__readonly(self, obj):
+        return self.get_readonly_blob_widget(obj, "example_create_only_blob")
+
+    example_create_only_blob__readonly.short_description = "Create only blob"
+
+    # You need to do this for each of the blob fields on the model
+    def example_read_only_blob__readonly(self, obj):
+        return self.get_readonly_blob_widget(obj, "example_read_only_blob")
+
+    example_read_only_blob__readonly.short_description = "Read only blob"
 
 
 django_admin.site.register(ExampleStorageModel, ExampleStorageModelAdmin)
 django_admin.site.register(ExampleBlobFieldModel, ExampleBlobFieldModelAdmin)
 django_admin.site.register(ExampleBlankBlobFieldModel, ExampleBlankBlobFieldModelAdmin)
-django_admin.site.register(ExampleUneditableBlobFieldModel, ExampleUneditableBlobFieldModelAdmin)
+django_admin.site.register(ExampleCallbackBlobFieldModel, ExampleCallbackBlobFieldModelAdmin)
+django_admin.site.register(ExampleNeverOverwriteBlobFieldModel, ExampleNeverOverwriteBlobFieldModelAdmin)
 django_admin.site.register(ExampleMultipleBlobFieldModel, ExampleMultipleBlobFieldModelAdmin)
+django_admin.site.register(ExamplePathValidationBlobFieldModel, ExamplePathValidationBlobFieldModelAdmin)
+django_admin.site.register(ExampleReadOnlyBlobFieldModel, ExampleReadOnlyBlobFieldModelAdmin)
--- tests/server/example/models.py
+++ tests/server/example/models.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+import os
 from uuid import uuid4
-from django.db.models import CharField, FileField, Model
+
+from django.core.exceptions import ValidationError
+from django.db.models import CharField, FileField, Model, UUIDField
+
 from django_gcp.storage.fields import BlobField
 
 
@@ -66,6 +71,72 @@ def get_destination_path(
     return f"{category}{random_prefix}-{original_name}", allow_overwrite
 
 
+def get_destination_path_from_id(
+    instance,
+    original_name,
+    attributes,
+    existing_path,
+    temporary_path,
+    allow_overwrite,
+    bucket,
+):
+    """A deterministic naming callback. This produces the same name for every file
+    which is very helpful for figuring out what things are in your object store, but
+    can have drawbacks when it comes to naming conflicts.
+
+    Here, the name is generated using the model id but could easily use any other unique
+    combination of fields in the model. Note that you can't do this with autoincrementing
+    primary IDs because this callback needs to run prior to save.
+
+    However, this can result in validation error for a range of cases - for example,
+    if you have `overwrite_mode=never` then attempt to change the file, the choice of
+    deterministic filename conflicts with that choice: the BlobField should be rendered
+    as a read-only widget to handle this.
+
+    This also demonstrates that it's possible to raise validation errors from
+    this callback; in this case for an unknown file extension
+    """
+    extension = os.path.splitext(original_name)[1]
+    return f"files/file-{instance.id}{extension}", allow_overwrite
+
+
+def get_destination_path_and_validate(
+    instance,
+    original_name,
+    attributes,
+    existing_path,
+    temporary_path,
+    allow_overwrite,
+    bucket,
+):
+    """Demonstrates validation error behaviour in the chosen naming path
+
+    NOTE: You can use this for other kinds of validation too, raising errors
+    when the file contents are invalid, for example.
+
+    HOWEVER, in most cases, you need to be wary. Typically, you're using this
+    field (instead of FileField) because the files are large and unwieldy.
+
+    Yes, you could download the blob, open it and validate its contents but
+    in most cases that would be too slow to happen within the request-response
+    cycle.
+
+    I have seen in one case someone streaming only the first few bytes of a file
+    to check it had the right headers, which is a creative approach to the problem.
+
+    In general though for validating the contents of large files, you'll want to
+    have an async tasks (or series of tasks) that run after the ingress - you'll
+    want to keep a set of events to store the progress of the file through the
+    validation process(es) and mark it as ready once complete. This is outside the scope
+    of django-gcp and is a more generic approach to building a data lake!
+    """
+
+    extension = os.path.splitext(original_name)[1]
+    if extension != ".txt":
+        raise ValidationError("The file needs to have a .txt extension")
+    return f"files/file-{instance.id}.txt", allow_overwrite
+
+
 class ExampleStorageModel(Model):
     """
     An example model containing a FileField with django's normal indirect
@@ -73,6 +144,27 @@ class ExampleStorageModel(Model):
     """
 
     normal = FileField(upload_to="normal_uploads", blank=True, null=True)
+
+    class Meta:
+        """Metaclass defining this model to reside in the example app"""
+
+        app_label = "example"
+
+
+class ExampleBlankBlobFieldModel(Model):
+    """
+    As ExampleBlobFieldModel but showing blankable behaviour
+    (This is mostly used for widget development and testing)
+    """
+
+    category = CharField(max_length=20, blank=True, null=True)
+    blob = BlobField(
+        get_destination_path=get_destination_path,
+        store_key="media",
+        blank=True,
+        null=True,
+        help_text="Shows typical behaviour of the blobfield, with name collision prevention so you should be able to change the file as many times as you like. The field is blankable (saving with no file selected should succeed).",
+    )
 
     class Meta:
         """Metaclass defining this model to reside in the example app"""
@@ -100,26 +192,10 @@ class ExampleBlobFieldModel(Model):
     #       blob_temp = BlobField(store_key="media")
     #   Then a custom migration is used to duplicate the data (see migration 0002).
     #   Then the old field is removed (see migration 0003).
-    blob = BlobField(get_destination_path=get_destination_path, store_key="media")
-
-    class Meta:
-        """Metaclass defining this model to reside in the example app"""
-
-        app_label = "example"
-
-
-class ExampleBlankBlobFieldModel(Model):
-    """
-    As ExampleBlobFieldModel but showing blankable behaviour
-    (This is mostly used for widget development and testing)
-    """
-
-    category = CharField(max_length=20, blank=True, null=True)
     blob = BlobField(
         get_destination_path=get_destination_path,
         store_key="media",
-        blank=True,
-        null=True,
+        help_text="Shows typical behaviour of the blobfield, with name collision prevention so you should be able to change the file as many times as you like, but it is not blankable or nullable (attempt to save with no file selected to see validation error).",
     )
 
     class Meta:
@@ -128,14 +204,79 @@ class ExampleBlankBlobFieldModel(Model):
         app_label = "example"
 
 
-class ExampleUneditableBlobFieldModel(Model):
+def my_on_change_callback(value, instance):
+    """Demonstrate the on_change callback functionality
+
+    This callback is changed AFTER the commit of the current transaction
+
+    It is designed for lightweight work like triggering further processing
+    tasks or sending emails, etc, which must be done only if the transaction
+    succeeded.
+
+    If you need to make a change to the model as a result of the field
+    preparation which must be included in the *same* transaction as the change
+    to your BlobField, you should override your model's save() method to
+    achieve this in the normal way (after the model's clean() method has been called)
+    and that is the recommended pattern in general.
+
+    The model instance is available here, but in an out-of-date state, so
+    if you must work with the model instance, you may have to call
+    refresh_from_db on it. You will also have to save any changes you make to the
+    instance.
     """
-    As ExampleBlobFieldModel but showing behaviour when editable=False
+    dt = datetime.now().isoformat()
+    instance.refresh_from_db()
+    # The on_change is called AFTER the commit of the current transaction, so
+    if value is None:
+        print("You removed the blob")
+        instance.activity = f"Removed blob on {dt}"
+    else:
+        print(f"Do something with the value {value} and instance {instance}")
+        instance.activity = f"Changed blob on {dt}"
+    instance.save()
+
+
+class ExampleCallbackBlobFieldModel(Model):
+    """
+    As ExampleBlobFieldModel but showing use of the on_change callback
     (This is mostly used for widget development and testing)
     """
 
     category = CharField(max_length=20, blank=True, null=True)
-    blob = BlobField(get_destination_path=get_destination_path, store_key="media", editable=False)
+    activity = CharField(max_length=60, blank=True, null=True)
+    blob = BlobField(
+        get_destination_path=get_destination_path,
+        store_key="media",
+        blank=True,
+        null=True,
+        on_change=my_on_change_callback,
+        help_text="The activity should update if you change the blob then save. If you save without changing the blob, activity shouldn't be affected.",
+    )
+
+    class Meta:
+        """Metaclass defining this model to reside in the example app"""
+
+        app_label = "example"
+
+
+class ExampleNeverOverwriteBlobFieldModel(Model):
+    """
+    As ExampleBlobFieldModel but showing behaviour when overwriting is disabled
+
+    Note that you should be able to create a model initially, but if you were to
+    edit the file and save you will get an AttemptedOverwriteError.
+
+    This is correct and expected - your admin or widget should be rendered as
+    read-only in order to prevent these exceptions occurring.
+    """
+
+    id = UUIDField(primary_key=True, default=uuid4, editable=False)
+    blob = BlobField(
+        get_destination_path=get_destination_path_from_id,
+        store_key="media",
+        overwrite_mode="never",
+        help_text="You can create a model like this, but if you attempt to select a different file during update, you should see an exception. You need to override the widget to show read-only in this case.",
+    )
 
     class Meta:
         """Metaclass defining this model to reside in the example app"""
@@ -154,9 +295,21 @@ class ExampleMultipleBlobFieldModel(Model):
     # in order to set the blob name
     category = CharField(max_length=20, blank=True, null=True)
 
-    blob1 = BlobField(get_destination_path=get_destination_path, store_key="media")
-    blob2 = BlobField(get_destination_path=get_destination_path, store_key="media")
-    blob3 = BlobField(get_destination_path=get_destination_path, store_key="media")
+    blob1 = BlobField(
+        get_destination_path=get_destination_path,
+        store_key="media",
+        help_text="The get_destination_path function gives these unique names",
+    )
+    blob2 = BlobField(
+        get_destination_path=get_destination_path,
+        store_key="media",
+        help_text="The get_destination_path function gives these unique names",
+    )
+    blob3 = BlobField(
+        get_destination_path=get_destination_path,
+        store_key="media",
+        help_text="The get_destination_path function gives these unique names",
+    )
 
     class Meta:
         """Metaclass defining this model to reside in the example app"""
@@ -164,27 +317,46 @@ class ExampleMultipleBlobFieldModel(Model):
         app_label = "example"
 
 
-def my_on_change_callback(value, instance):
-    """Demonstrate the on_change callback functionality"""
-    if value is None:
-        print("You removed the blob")
-    else:
-        print(f"Do something with the value {value} and instance {instance}")
+class ExamplePathValidationBlobFieldModel(Model):
+    """An ExampleBlobFieldModel but showing behaviour when a validation error is raised from get_destination_path"""
+
+    id = UUIDField(primary_key=True, default=uuid4, editable=False)
+    blob = BlobField(
+        get_destination_path=get_destination_path_and_validate,
+        store_key="media",
+        overwrite_mode="never",
+        help_text="If you upload a file that doesn't end with .txt, you should see a validation error in the admin.",
+    )
+
+    class Meta:
+        """Metaclass defining this model to reside in the example app"""
+
+        app_label = "example"
 
 
-class ExampleCallbackBlobFieldModel(Model):
+class ExampleReadOnlyBlobFieldModel(Model):
     """
-    As ExampleBlobFieldModel but showing use of the on_change callback
-    (This is mostly used for widget development and testing)
+    This is identical to ExampleBlobFieldModel but I've duplicated it because
+    it's complicated to register the same model twice in the admin, and we want to
+    show how to set up the admin with read-only fields
     """
 
     category = CharField(max_length=20, blank=True, null=True)
-    blob = BlobField(
+
+    example_read_only_blob = BlobField(
         get_destination_path=get_destination_path,
         store_key="media",
-        blank=True,
-        null=True,
-        on_change=my_on_change_callback,
+        help_text="The widget should be read-only always",
+        blank=True,  # So we can demo read-only behaviour
+        null=True,  # in the admin without creating blobs
+    )
+
+    example_create_only_blob = BlobField(
+        get_destination_path=get_destination_path,
+        store_key="media",
+        help_text="The widget should be as normal for object addition, read-only in a change form",
+        blank=True,  # So we can demo read-only behaviour
+        null=True,  # in the admin without creating blobs
     )
 
     class Meta:
