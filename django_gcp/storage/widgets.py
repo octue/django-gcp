8eafc1cce3231d92cca8ea2ceb1e3ae7d63ee313
Tom Clark <tom@octue.com>
2025-03-02 10:53:43 +0000

    FIX: Widget correctly accepts download_url and console_url

--- django_gcp/storage/widgets.py
+++ django_gcp/storage/widgets.py
@@ -1,7 +1,7 @@
 import json
+
 from django.conf import settings
 from django.forms import Widget
-
 
 DEFAULT_ACCEPT_MIMETYPE = "*/*"
 
@@ -45,17 +45,19 @@ class CloudObjectWidget(Widget):
     def get_context(self, name, value, attrs):
         context = super().get_context(name, value, attrs)
 
-        # if self.is_initial():
-        #     context["my_context"] = "Adding a new model"
-        # else:
-        #     context["my_context"] = "Changing an existing model"
-
         context["signed_ingress_url"] = self.signed_ingress_url
         context["ingress_path"] = self.ingress_path
         context["accept_mimetype"] = self.accept_mimetype
+
+        # Attach these to the instantiated widget in the ModelForm if you
+        # want download and console buttons to appear
+        if hasattr(self, "console_url"):
+            context["console_url"] = self.console_url
+
+        if hasattr(self, "download_url"):
+            context["download_url"] = self.download_url
+
         # Yes I know this is a hack but I've been working on this for weeks and django's a nightmare
         value_or_dict = json.loads(value) or {}
         context["existing_path"] = value_or_dict.get("path", "")
-        # context["signing_token"] = a token
-        # context["signing_url"] = submit the token to this url in exchange for a signed URL reverse("gcp-storage-get-direct-upload-url")
         return context
