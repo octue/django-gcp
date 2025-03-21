6168556fb1e58928ff3f63ddd0dbef3eda16eac9
Tom Clark <tom@octue.com>
2025-03-21 16:03:07 +0000

    ENH: Add BlobField to storage module exports

--- django_gcp/storage/__init__.py
+++ django_gcp/storage/__init__.py
@@ -1,16 +1,18 @@
 # TODO: Refactor tests to import members directly then remove this module export
 from . import gcloud
 from .blob_utils import BlobFieldMixin, get_blob, get_blob_name, get_path, get_signed_url
+from .fields import BlobField
 from .gcloud import GoogleCloudFile, GoogleCloudMediaStorage, GoogleCloudStaticStorage, GoogleCloudStorage
 from .operations import upload_blob
 
 __all__ = [
+    "BlobField",
+    "BlobFieldMixin",
     "GoogleCloudStorage",
     "GoogleCloudFile",
     "gcloud",
     "GoogleCloudMediaStorage",
     "GoogleCloudStaticStorage",
-    "BlobFieldMixin",
     "get_blob",
     "get_blob_name",
     "get_path",
