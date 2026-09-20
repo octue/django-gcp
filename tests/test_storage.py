d274d30f0a7029b7f20e2b9dde472f7a8925a3c8
Tom Clark <tom@octue.com>
2026-09-20 11:33:08 +0000

    TST: Cover application of per-store STORAGES options to extra stores
    The bug reported in #39 (options in GCP_STORAGE_EXTRA_STORES ignored, signed
    URLs returned for public stores) was resolved when store configuration moved
    to Django's STORAGES setting. Pin the fixed behaviour with regression tests
    instantiating a non-default store purely from settings.
    
    Closes #39

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -636,3 +636,72 @@ class GCloudStorageClassTests(GCloudTestCase):
 
         with self.assertRaises(ValueError):
             gcloud.GoogleCloudStaticStorage(store_key="not-staticfiles")
+
+
+class GCloudExtraStoreSettingsTests(GCloudTestCase):
+    """Regression tests for https://github.com/octue/django-gcp/issues/39
+
+    Options set on a non-default store alias in the STORAGES setting must be applied
+    to a storage instantiated with that store_key, without repeating those options as
+    constructor overrides on the field's storage instance.
+    """
+
+    def _storages_with_public_store(self, options):
+        storages = storages_with_default({"bucket_name": "test-media"})
+        storages["extra-public"] = {
+            "BACKEND": "django_gcp.storage.GoogleCloudStorage",
+            "OPTIONS": options,
+        }
+        return storages
+
+    def test_extra_store_options_are_applied(self):
+        storages = self._storages_with_public_store(
+            {
+                "bucket_name": "public-bucket",
+                "default_acl": "publicRead",
+                "querystring_auth": False,
+            }
+        )
+        with override_settings(STORAGES=storages):
+            storage = gcloud.GoogleCloudStorage(store_key="extra-public")
+            self.assertEqual(storage.settings.bucket_name, "public-bucket")
+            self.assertEqual(storage.settings.default_acl, "publicRead")
+            self.assertFalse(storage.settings.querystring_auth)
+
+    def test_extra_store_url_is_unsigned_with_public_acl_only(self):
+        """A publicRead ACL alone (querystring_auth left at its default) suffices for unsigned URLs"""
+        storages = self._storages_with_public_store(
+            {
+                "bucket_name": "public-bucket",
+                "default_acl": "publicRead",
+            }
+        )
+        with override_settings(STORAGES=storages):
+            storage = gcloud.GoogleCloudStorage(store_key="extra-public")
+            storage._bucket = mock.MagicMock()
+            blob = mock.MagicMock()
+            blob.public_url = "http://public_url"
+            blob.generate_signed_url = mock.MagicMock(return_value="http://signed_url")
+            storage._bucket.blob.return_value = blob
+
+            self.assertEqual(storage.url("image.png"), "http://public_url")
+            blob.generate_signed_url.assert_not_called()
+
+    def test_extra_store_url_is_unsigned_with_querystring_auth_only(self):
+        """Disabling querystring_auth alone (without a public ACL) suffices for unsigned URLs"""
+        storages = self._storages_with_public_store(
+            {
+                "bucket_name": "public-bucket",
+                "querystring_auth": False,
+            }
+        )
+        with override_settings(STORAGES=storages):
+            storage = gcloud.GoogleCloudStorage(store_key="extra-public")
+            storage._bucket = mock.MagicMock()
+            blob = mock.MagicMock()
+            blob.public_url = "http://public_url"
+            blob.generate_signed_url = mock.MagicMock(return_value="http://signed_url")
+            storage._bucket.blob.return_value = blob
+
+            self.assertEqual(storage.url("image.png"), "http://public_url")
+            blob.generate_signed_url.assert_not_called()
